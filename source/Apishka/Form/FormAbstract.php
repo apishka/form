034e6436a20428e05baf0fce46db908ba0172b9e
Eugene Reich <eugene.reich@gmail.com>
2016-02-28 22:10:14 +0400

    Added abstract form class

---
+++ source/Apishka/Form/FormAbstract.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Apishka form form abstract
+ *
+ * @abstract
+ * @author Evgeny Reykh <evgeny@reykh.com>
+ */
+
+abstract class Apishka_Form_FormAbstract
+{
+    /**
+     * Traits
+     */
+
+    use \Apishka\EasyExtend\Helper\ByClassNameTrait;
+}
