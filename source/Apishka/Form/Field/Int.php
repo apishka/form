75780e4fbc03602320c92c84bfb7571252489e34
Eugene Reich <eugene.reich@gmail.com>
2016-02-28 22:06:03 +0400

    Added default fields

---
+++ source/Apishka/Form/Field/Int.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Apishka form field int
+ *
+ * @uses Apishka_Form_FieldAbstract
+ *
+ * @author Evgeny Reykh <evgeny@reykh.com>
+ */
+
+class Apishka_Form_Field_Int extends Apishka_Form_FieldAbstract
+{
+}
---
+++ source/Apishka/Form/FieldAbstract.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * Apishka form field abstract
+ *
+ * @abstract
+ * @author Evgeny Reykh <evgeny@reykh.com>
+ */
+
+abstract class Apishka_Form_FieldAbstract
+{
+    /**
+     * Traits
+     */
+
+    use \Apishka\EasyExtend\Helper\ByClassNameTrait;
+
+    /**
+     * Options
+     *
+     * @var array
+     */
+
+    private $_options = array();
+
+    /**
+     * Construct
+     */
+
+    public function __construct()
+    {
+        $this->_options = $this->getDefaultOptions();
+    }
+
+    /**
+     * Get default options
+     *
+     * @return array
+     */
+
+    protected function getDefaultOptions()
+    {
+        return array(
+            'default'   => null,
+            'required'  => false,
+        );
+    }
+
+    /**
+     * Set required
+     *
+     * @param bool $required
+     *
+     * @return Apishka_Form_FieldAbstract this
+     */
+
+    public function setRequired($required)
+    {
+        return $this->setOption('required', (bool) $required);
+    }
+
+    /**
+     * Get required
+     *
+     * @return bool
+     */
+
+    public function getRequired()
+    {
+        return $this->getOption('required');
+    }
+
+    /**
+     * Set default
+     *
+     * @param mixed $value
+     *
+     * @return Apishka_Form_FieldAbstract this
+     */
+
+    public function setDefault($value)
+    {
+        return $this->setOption('default', $value);
+    }
+
+    /**
+     * Get value
+     *
+     * @return void
+     */
+
+    public function getDefault()
+    {
+        return $this->getOption('default');
+    }
+
+    /**
+     * Set option
+     *
+     * @param string $name
+     * @param mixed  $value
+     *
+     * @return Apishka_Form_FieldAbstract this
+     */
+
+    protected function setOption($name, $value)
+    {
+        $this->_options[$name] = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get option
+     *
+     * @param string $name
+     *
+     * @return mixed
+     */
+
+    protected function getOption($name)
+    {
+        return $this->_options[$name];
+    }
+}
