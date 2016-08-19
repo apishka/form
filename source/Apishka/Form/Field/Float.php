5ddfa11584d86f4b54861de06a554b5054a39004
Eugene Reich <eugene.reich@gmail.com>
2016-08-19 22:42:06 +0400

    Added test on float fields

---
+++ source/Apishka/Form/Field/Float.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Apishka form field float
+ */
+
+class Apishka_Form_Field_Float extends Apishka_Form_FieldAbstract
+{
+    /**
+     * Get default transformations
+     *
+     * @return array
+     */
+
+    protected function getDefaultTransformations()
+    {
+        $transformations = array();
+
+        $transformations['Transform/Blank'] = [];
+        $transformations['Transform/NotBlank'] = ['condition' => function () {return $this->getRequired();}];
+        $transformations['Transform/Float'] = [];
+        $transformations['Transform/ArrayKeyExists'] = ['condition' => function () {return $this->getValues() !== null;}, 'values' => function () {return $this->__getValues();}];
+
+        return $transformations;
+    }
+}
---
+++ tests/ApishkaTest/Form/Field/FloatTest.php
@@ -0,0 +1,353 @@
+<?php
+
+/**
+ * Apishka test form field float test
+ */
+
+class ApishkaTest_Form_Field_FloatTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Get form
+     *
+     * @param bool $is_sent
+     */
+
+    protected function getForm($is_sent)
+    {
+        $stub = $this->getMockBuilder('Apishka_Form_FormAbstract')
+            ->setMockClassName('ApishkaTest_Form_Field_FloatTest_Form')
+            ->setMethods(['isSent'])
+            ->getMock()
+        ;
+
+        $stub->expects($this->any())
+            ->method('isSent')
+            ->will($this->returnValue($is_sent))
+        ;
+
+        return $stub;
+    }
+
+    /**
+     * Get field
+     *
+     * @param string $name
+     * @param bool   $is_sent
+     *
+     * @return Apishka_Form_Field_Signature
+     */
+
+    protected function getField($name, $is_sent = true)
+    {
+        $field = Apishka_Form_Field_Float::apishka($name);
+        $field->initialize($this->getForm($is_sent));
+
+        return $field;
+    }
+
+    /**
+     * Test name
+     */
+
+    public function testName()
+    {
+        $field = $this->getField('float_filed');
+
+        $this->assertSame(
+            'float_filed',
+            $field->getName()
+        );
+
+        $this->assertSame(
+            'float_filed',
+            $field->name
+        );
+
+        $this->assertSame(
+            'float_filed',
+            $field->getStructureName()
+        );
+
+        $field->setName('float_filed_2');
+
+        $this->assertSame(
+            'float_filed_2',
+            $field->getName()
+        );
+
+        $this->assertSame(
+            'float_filed_2',
+            $field->name
+        );
+
+        $this->assertSame(
+            'float_filed',
+            $field->getStructureName()
+        );
+    }
+
+    /**
+     * Test value with empty request
+     */
+
+    public function testValueWithEmptyRequest()
+    {
+        $field = $this->getField('float_filed');
+
+        $this->assertNull($field->getValue());
+
+        $this->assertTrue($field->isValid());
+        $this->assertNull($field->value);
+    }
+
+    /**
+     * Test value
+     *
+     * @backupGlobals enabled
+     * @expectedException Apishka\Transformer\FriendlyException
+     * @expectedExceptionMessage wrong input format
+     */
+
+    public function testValueWithInvalidRequest()
+    {
+        $field = $this->getField('float_filed');
+
+        $_REQUEST = array(
+            $field->name => 'edfaed6b51dedc42b21d58134f1afe93',
+        );
+
+        $this->assertFalse($field->isValid());
+
+        throw $field->getError();
+    }
+
+    /**
+     * Test value
+     *
+     * @backupGlobals enabled
+     */
+
+    public function testValueWithRequest()
+    {
+        $field = $this->getField('float_filed');
+
+        $_REQUEST = array(
+            $field->name => '100.1',
+        );
+
+        $this->assertTrue($field->isValid());
+        $this->assertSame(
+            100.1,
+            $field->value
+        );
+    }
+
+    /**
+     * Test not required
+     */
+
+    public function testNotRequired()
+    {
+        $field = $this->getField('float_filed');
+
+        $this->assertTrue($field->isValid());
+        $this->assertNull($field->value);
+    }
+
+    /**
+     * Test required
+     *
+     * @expectedException \Apishka\Transformer\FriendlyException
+     * @expectedExceptionMessage cannot be empty
+     */
+
+    public function testRequired()
+    {
+        $field = $this->getField('float_filed');
+        $field->setRequired(true);
+
+        $this->assertFalse($field->isValid());
+
+        throw $field->getError();
+    }
+
+    /**
+     * Test blank value
+     *
+     * @backupGlobals enabled
+     */
+
+    public function testBlankValue()
+    {
+        $field = $this->getField('float_filed');
+
+        $_REQUEST = array(
+            $field->name => '',
+        );
+
+        $this->assertTrue($field->isValid());
+        $this->assertNull($field->value);
+    }
+
+    /**
+     * Test default value
+     */
+
+    public function testDefaultValue()
+    {
+        $field = $this->getField('float_filed');
+        $field->setDefault(1.0);
+
+        $this->assertTrue($field->isValid());
+        $this->assertSame(
+            1.0,
+            $field->value
+        );
+    }
+
+    /**
+     * Test default value with blank request
+     *
+     * @backupGlobals enabled
+     */
+
+    public function testDefaultValueWithBlankRequest()
+    {
+        $field = $this->getField('float_filed');
+        $field->setDefault(1);
+
+        $_REQUEST = array(
+            $field->name => '',
+        );
+
+        $this->assertTrue($field->isValid());
+        $this->assertNull($field->value);
+    }
+
+    /**
+     * Test default required value with blank request
+     *
+     * @backupGlobals            enabled
+     * @expectedException        \Apishka\Transformer\FriendlyException
+     * @expectedExceptionMessage cannot be empty
+     */
+
+    public function testDefaultRequiredValueWithBlankRequest()
+    {
+        $field = $this->getField('float_filed');
+        $field->setRequired(true);
+        $field->setDefault(1);
+
+        $_REQUEST = array(
+            $field->name => '',
+        );
+
+        $this->assertFalse($field->isValid());
+
+        throw $field->getError();
+    }
+
+    /**
+     * Test default value for non sent form
+     */
+
+    public function testDefaultValueForNonSentForm()
+    {
+        $field = $this->getField('float_filed', false);
+        $field->setDefault(100);
+
+        $this->assertTrue($field->isValid());
+        $this->assertSame(
+            100,
+            $field->value
+        );
+    }
+
+    /**
+     * Test good values
+     *
+     * @dataProvider  goodValuesProvider
+     * @backupGlobals enabled
+     *
+     * @param mixed $value
+     * @param array $values
+     */
+
+    public function testGoodValues($value, $values)
+    {
+        $field = $this->getField('float_filed')
+            ->setValues($values)
+        ;
+
+        $_REQUEST = array(
+            $field->name => $value,
+        );
+
+        $this->assertTrue($field->isValid());
+        $this->assertSame(
+            (float) $value,
+            $field->value
+        );
+    }
+
+    /**
+     * Good data provider
+     *
+     * @return array
+     */
+
+    public function goodValuesProvider()
+    {
+        return array(
+            array(1, ['1' => 'test']),
+            array(-1, ['-1' => 'test']),
+            array(true, ['1' => 'test']),
+            array('1', function () {return array(1 => 123);}),
+        );
+    }
+
+    /**
+     * Test good values
+     *
+     * @dataProvider             badValuesProvider
+     * @backupGlobals            enabled
+     * @expectedException        \Apishka\Transformer\Exception
+     * @expectedExceptionMessage wrong input format
+     *
+     * @param mixed $value
+     * @param array $values
+     */
+
+    public function testBadValues($value, $values)
+    {
+        $field = $this->getField('float_filed')
+            ->setValues($values)
+        ;
+
+        $_REQUEST = array(
+            $field->name => $value,
+        );
+
+        $this->assertFalse($field->isValid());
+
+        throw $field->getError();
+    }
+
+    /**
+     * Good data provider
+     *
+     * @return array
+     */
+
+    public function badValuesProvider()
+    {
+        return array(
+            array(1, ['test' => 'test']),
+            array(1.2, ['test' => 'test']),
+            array(true, ['test' => 'test']),
+            array('test', ['test1' => 'test1']),
+            array(function () {}, ['test' => 'test']),
+            array(new \StdClass(), ['test' => 'test']),
+        );
+    }
+}
